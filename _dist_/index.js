2e57fcb5adc7d67f2aa3dc7b678f5b0c6bb81c76
ODCenteno <ODCenteno@users.noreply.github.com>
2022-09-07 02:52:47 +0000

    deploy: 077d791a8314027b2d9c10a3bb1dd4c442bbe4e0

---
+++ _dist_/index.js
@@ -0,0 +1,25 @@
+import { registerImage } from './lazy.js';
+
+const addImageBtn = document.querySelector('#add-image');
+const appImg = document.querySelector('#images');
+const baseURLPic = 'https://randomfox.ca/images/';
+const imgQuantity = 3;
+const foxImagesURLs = [];
+
+addImageBtn.addEventListener('click', addImage);
+
+let number = () => {
+    const randInt = Math.floor(Math.random() * (123 - 1 + 1) + 1 )
+    console.log(`Random number is: ${randInt}`)
+    return randInt;
+}
+
+function addImage() {
+    const image = document.createElement('img');
+    image.className = "mx-auto p-1";
+    image.width = "320";
+    image.src = `${baseURLPic}${number()}.jpg`;
+
+    appImg.appendChild(image);
+    registerImage(image);
+}
